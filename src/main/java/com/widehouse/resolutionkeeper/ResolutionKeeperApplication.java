faf369a0fb523d53ce6e82de002d08adc2c9ae2b
kielhong <kielhong@naver.com>
2019-12-21 20:22:38 +0900

    init project

---
+++ src/main/java/com/widehouse/resolutionkeeper/ResolutionKeeperApplication.java
@@ -0,0 +1,12 @@
+package com.widehouse.resolutionkeeper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResolutionKeeperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResolutionKeeperApplication.class, args);
+    }
+}
---
+++ src/test/java/com/widehouse/resolutionkeeper/ResolutionKeeperApplicationTests.java
@@ -0,0 +1,12 @@
+package com.widehouse.resolutionkeeper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResolutionKeeperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
