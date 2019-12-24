11bb08a4a433f3c19914d504e601cc1a9cf42cf5
kielhong <kielhong@naver.com>
2019-12-24 12:51:55 +0900

    #4 auto generated createdDate, ModifiedDate

--- src/main/java/com/widehouse/resolutionkeeper/resolution/domain/Resolution.java
+++ src/main/java/com/widehouse/resolutionkeeper/resolution/domain/Resolution.java
@@ -1,14 +1,18 @@
 package com.widehouse.resolutionkeeper.resolution.domain;
 
+import java.time.Instant;
+import java.time.ZonedDateTime;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
+import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
@@ -19,4 +23,19 @@ public class Resolution {
     private String id;
     private String name;
     private String description;
+
+    @CreatedDate
+    private Instant createdAt;
+    @LastModifiedDate
+    private Instant updatedAt;
+
+    @Builder
+    private static Resolution of(String id, String name, String description) {
+        Resolution resolution = new Resolution();
+        resolution.id = id;
+        resolution.name = name;
+        resolution.description = description;
+
+        return resolution;
+    }
 }
--- src/test/java/com/widehouse/resolutionkeeper/resolution/domain/ResolutionRepositoryTest.java
+++ src/test/java/com/widehouse/resolutionkeeper/resolution/domain/ResolutionRepositoryTest.java
@@ -2,13 +2,17 @@ package com.widehouse.resolutionkeeper.resolution.domain;
 
 import static org.assertj.core.api.BDDAssertions.then;
 
+import com.widehouse.resolutionkeeper.config.MongoConfig;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+import org.springframework.context.annotation.Import;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
 @DataMongoTest
+@Import(MongoConfig.class)
 class ResolutionRepositoryTest {
     @Autowired
     private ResolutionRepository repository;
@@ -25,12 +29,13 @@ class ResolutionRepositoryTest {
         // then
         StepVerifier
                 .create(actual)
-                .assertNext(v -> {
-                    then(v)
-                            .hasFieldOrProperty("id")
-                            .hasFieldOrPropertyWithValue("name", "resolution")
-                            .hasFieldOrPropertyWithValue("description", "desc");
-                })
+                .assertNext(result -> then(result)
+                        .hasFieldOrProperty("id")
+                        .hasFieldOrPropertyWithValue("name", "resolution")
+                        .hasFieldOrPropertyWithValue("description", "desc")
+                        .hasFieldOrProperty("createdAt")
+                        .hasFieldOrProperty("updatedAt")
+                )
                 .expectComplete()
                 .verify();
     }
